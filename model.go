ae90a504f16e3358e4648c2d643bee527c764ebc
izumin5210 <m@izum.in>
2017-12-03 04:02:04 +0900

    minor fix

--- model.go
+++ model.go
@@ -6,12 +6,12 @@ type Model struct {
 
 // GetKeySuffix implements the types.Model interface.
 // If you does not override this function or return empty string, it will be returned errors.
-func (b *Model) GetKeySuffix() string {
+func (m *Model) GetKeySuffix() string {
 	return ""
 }
 
 // GetScoreMap implements the types.Model interface.
 // If you does not override this function, ro.Store does not store any scores.
-func (b *Model) GetScoreMap() map[string]interface{} {
+func (m *Model) GetScoreMap() map[string]interface{} {
 	return map[string]interface{}{}
 }
